32a0fe154032444804b50344f99ab5d7a79c5d0b
Anukul <shenoyanukul@gmail.com>
2017-04-11 11:19:20 -0700

    Added test file Created max duration function to get longest preview duration

---
+++ server/server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const Promise = require("bluebird");
+const request = Promise.promisify(require('request'));
+var tid = 26681;
+var longestPreviewMedia = {};
+
+var options = {
+    url: 'http://d6api.gaia.com/vocabulary/1/' + tid,
+    method: 'GET',
+    headers: {
+        'Accept': 'application/json'
+    }
+};
+var maxDuration = function(array){
+    var max, previewNid, titleNid;
+    for (var i = 0; i < array.length; i++) {
+        if (!max) {
+            max = Number(array[0].preview.duration);
+            previewNid = array[0].preview.nid;
+            titleNid = array[0].feature.nid;
+        }
+        if (array[i].preview && Number(array[i].preview.duration) > max) {
+                max = Number(array[i].preview.duration);
+                previewNid = array[i].preview.nid;
+                titleNid = array[i].feature.nid;
+            }
+
+    }
+    return {"max" : max, "previewNid": previewNid, "titleNid": titleNid};
+}
+
+app.get('/', function (req, res) {
+    request(options).then(function(data){
+        tid = JSON.parse(data.body).terms[0].tid;
+        options.url = 'http://d6api.gaia.com/videos/term/' + tid;
+        request(options).then(function(data) {
+            var videoData = maxDuration(JSON.parse(data.body).titles);
+            longestPreviewMedia.previewDuration = videoData.max;
+            longestPreviewMedia.previewNid = videoData.previewNid;
+            longestPreviewMedia.titleNid = videoData.titleNid;
+            options.url = 'http://d6api.gaia.com/media/' + videoData.previewNid;
+            request(options).then(function(data){
+                longestPreviewMedia.bcHLS = JSON.parse(data.body).mediaUrls.bcHLS;
+                res.send(JSON.stringify(longestPreviewMedia));
+            })
+        })
+
+    });
+});
+
+app.listen(3000, function () {
+    console.log('Listening on port 3000!')
+});
